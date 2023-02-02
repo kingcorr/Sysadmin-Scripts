f721798ca5e3f97e7284ab1204bd13358d17075a
ryan <ryan@fannon.net>
2023-02-02 11:05:26 -0700

    added bat files

---
+++ Bat Files/Clear Cached Credentials.bat
@@ -0,0 +1,4 @@
+cmdkey.exe /list > "%TEMP%\List.txt"
+findstr.exe Target "%TEMP%\List.txt" > "%TEMP%\tokensonly.txt"
+FOR /F "tokens=1,2 delims= " %%G IN (%TEMP%\tokensonly.txt) DO cmdkey.exe /delete:%%H
+del "%TEMP%\*.*" /s /f /q
